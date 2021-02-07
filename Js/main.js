bff15e42a96267b6101e7ee184e87b696a460d39
Jeremyispapo <jeremyispapo@gmail.com>
2021-02-07 09:32:31 -0500

    Añadí texto en cada interaccion con los botones //Proyecto terminado//

--- Js/main.js
+++ Js/main.js
@@ -3,15 +3,23 @@ const timon = document.querySelector('#zTimon')
 const asiento = document.querySelector('#zAsiento')
 const aro = document.querySelector('#zAro')
 const img = document.querySelector('#Img')
+// Articulos
+const A_pedal = document.querySelector('.pedal');
+const A_timon = document.querySelector('.timon')
+const A_asiento = document.querySelector('.asiento')
+const A_aro = document.querySelector('.aro')
 
 pedal.addEventListener('click',zoomPedal);
 timon.addEventListener('click',zoomTimon);
 asiento.addEventListener('click',zoomAsiento);
 aro.addEventListener('click',zoomAro);
 
+// Funciones de Articulos
+
 function zoomPedal(){
     img.classList.toggle('pedal');
     pedal.classList.toggle('active');
+    A_pedal.classList.toggle('active');
     timon.classList.toggle('disable');
     aro.classList.toggle('disable');
     asiento.classList.toggle('disable');
@@ -21,6 +29,7 @@ function zoomTimon(){
     pedal.classList.toggle('disable');
     img.classList.toggle('timon');
     timon.classList.toggle('active');
+    A_timon.classList.toggle('active');
     aro.classList.toggle('disable');
     asiento.classList.toggle('disable');
 }
@@ -30,6 +39,7 @@ function zoomAsiento(){
     timon.classList.toggle('disable');
     img.classList.toggle('asiento');
     asiento.classList.toggle('active');
+    A_asiento.classList.toggle('active');
     aro.classList.toggle('disable');
 }
 
@@ -39,4 +49,5 @@ function zoomAro(){
     asiento.classList.toggle('disable');
     img.classList.toggle('aro');
     aro.classList.toggle('active');
+    A_aro.classList.toggle('active');
 }
